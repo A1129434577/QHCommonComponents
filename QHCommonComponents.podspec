b933fce8de5d2cc79d90b5d78d377528994317f0
刘彬 <1129434577@qq.com>
2019-07-16 17:19:51 +0800

    no message

--- QHCommonComponents.podspec
+++ QHCommonComponents.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'QHCommonComponents'
-  s.version          = '0.2.5'
+  s.version          = '0.2.6'
   s.summary          = 'QHCommonComponents of QHWL project.'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
--- QHCommonComponents/QHNSObjects/NSObject+SVProgressHUD/NSObject+SVProgressHUD.m
+++ QHCommonComponents/QHNSObjects/NSObject+SVProgressHUD/NSObject+SVProgressHUD.m
@@ -10,33 +10,70 @@
 
 @implementation NSObject (SVProgressHUD)
 - (void)showWithStatus:(NSString*)status {
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = NO;
     [SVProgressHUD dismiss];
     [SVProgressHUD showWithStatus:status];
-    
 }
 - (void)showSuccessWithStatus:(NSString *_Nullable)status{
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = NO;
     [SVProgressHUD dismiss];
     [SVProgressHUD showSuccessWithStatus:status];
+    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:^{
+        [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
+    }];
 }
 - (void)showSuccessWithStatus:(NSString *_Nullable)status completion:(SVProgressHUDDismissCompletion)completion{
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = NO;
+    [SVProgressHUD dismiss];
     [SVProgressHUD showSuccessWithStatus:status];
-    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:completion];
+    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:^{
+        completion?completion():NULL;
+        [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
+    }];
 }
 
 - (void)showErrorWithStatus:(NSString*)status {
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = NO;
     [SVProgressHUD dismiss];
     [SVProgressHUD showErrorWithStatus:status];
+    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:^{
+        [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
+    }];
 }
 
 - (void)showErrorWithStatus:(NSString *_Nullable)status completion:(SVProgressHUDDismissCompletion)completion{
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = NO;
+    [SVProgressHUD dismiss];
     [SVProgressHUD showErrorWithStatus:status];
-    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:completion];
+    [SVProgressHUD dismissWithDelay:[SVProgressHUD displayDurationForString:status] completion:^{
+        completion?completion():NULL;
+        [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
+    }];
 }
 
 - (void)dismiss{
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
     [SVProgressHUD dismiss];
 }
 - (void)dismissWithCompletion:(SVProgressHUDDismissCompletion)completion{
+    [NSObject topViewControllerWithRootViewController:[UIApplication sharedApplication].delegate.window.rootViewController].view.userInteractionEnabled = YES;
     [SVProgressHUD dismissWithCompletion:completion];
+}
+
+
++ (UIViewController *)topViewControllerWithRootViewController:(UIViewController *)rootVC
+{
+    if ([rootVC isKindOfClass:[UITabBarController class]]) {
+        UITabBarController *tabBarController = (UITabBarController*)rootVC;
+        return (UIViewController *)[self topViewControllerWithRootViewController:tabBarController.selectedViewController];
+    } else if ([rootVC isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = (UINavigationController*)rootVC;
+        return (UIViewController *)[self topViewControllerWithRootViewController:(UIViewController *)navigationController.visibleViewController];
+    } else if (rootVC.presentedViewController) {
+        UIViewController *presentedViewController = (UIViewController *)rootVC.presentedViewController;
+        return (UIViewController *)[self topViewControllerWithRootViewController:presentedViewController];
+    } else {
+        return rootVC;
+    }
 }
 @end
